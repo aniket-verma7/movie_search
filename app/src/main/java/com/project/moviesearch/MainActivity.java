caa4c48ba4b69ed9db6a0927e9d3d127a4eea8d0
Aniket Verma <aniketv2000@gmail.com>
2022-05-10 13:25:38 +0530

    Refactor

--- app/src/main/java/com/project/moviesearch/MainActivity.java
+++ app/src/main/java/com/project/moviesearch/MainActivity.java
@@ -42,10 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         init();
-
-
     }
 
     private void init() {
